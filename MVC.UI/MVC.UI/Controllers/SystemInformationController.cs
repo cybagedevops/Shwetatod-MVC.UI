0efea4b80b23094d2ddf7fd740733ec1725bb326
Shweta <shwetatod@cybage.com>
2021-12-03 13:34:10 +0530

    updated web-config

--- MVC.UI/MVC.UI/Controllers/SystemInformationController.cs
+++ MVC.UI/MVC.UI/Controllers/SystemInformationController.cs
@@ -1,24 +1,55 @@
-﻿using System.Collections.Generic;
-using System.Web.Mvc;
-
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Web.Mvc;
+using System.Net.Http.Headers;
+using System.Web.Configuration;
 using WebAPIHandson.Models;
 
-namespace MVC.UI.Controllers
+namespace WebMvcSystemInfo.Controllers
 {
     public class SystemInformationController : Controller
     {
-        // GET: SystemInformation         // GET: Home
+
+        public HttpClient WebCilent = new HttpClient();
+        // GET: SystemInformation
+
+
+
+        // GET: Home
         public ActionResult Index()
         {
-            //IEnumerable<SystemInformation> emptList; HttpResponseMessage response = Globalvariables.WebCilent.GetAsync("SystemInformation").Result;
-        
-           //emptList = response.Content.ReadAsAsync<IEnumerable<SystemInformation>>().Result; return View(emptList);
-            IEnumerable<SystemInformation> emptList; HttpResponseMessage response = Globalvariables.WebCilent.GetAsync("SystemInformation").Result;
-            emptList = response.Content.ReadAsAsync<IEnumerable<SystemInformation>>().Result; 
+            IEnumerable<SystemInformation> emptList;
+
+
+
+
+            string apiUrl = WebConfigurationManager.AppSettings["ApiUrl"];
+
+
+
+            WebCilent.BaseAddress = new Uri(apiUrl);
+
+
+
+            WebCilent.DefaultRequestHeaders.Clear();
+
+
+
+            WebCilent.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+
+            HttpResponseMessage response = WebCilent.GetAsync("SystemInformation").Result;
+            emptList = response.Content.ReadAsAsync<IEnumerable<SystemInformation>>().Result;
+
+
+
             return View(emptList);
         }
-    }
+
+
 
 
     }
+}
