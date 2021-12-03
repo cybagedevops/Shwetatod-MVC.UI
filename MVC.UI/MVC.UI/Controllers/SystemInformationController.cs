2d1f535cc9d68d64001ab8bd37d101c8ace9df7e
Shweta Nitin Todkar <shwetatod@cybage.com>
2021-12-03 06:42:38 +0000

    Updated SystemInformationController.cs

--- MVC.UI/MVC.UI/Controllers/SystemInformationController.cs
+++ MVC.UI/MVC.UI/Controllers/SystemInformationController.cs
@@ -1,18 +1,66 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using MVC.UI.Models;
+using System.Net.Http.Headers;
+
+
 
 using System.Net.Http;
-using WebAPIHandson.Models;
+using System.Web.Configuration;
 
-namespace MVC.UI.Controllers
+
+
+namespace WebMvcSystemInfo.Controllers
 {
-    public class SystemInformationController : Controller
-    {
-        // GET: SystemInformation         // GET: Home
-        public ActionResult Index()
-        {
-            IEnumerable<SystemInformation> emptList; HttpResponseMessage response = Globalvariables.WebCilent.GetAsync("SystemInformation").Result;
-            emptList = response.Content.ReadAsAsync<IEnumerable<SystemInformation>>().Result; return View(emptList);
-        }
-    }
+public class SystemInformationController : Controller
+{
+
+
+
+public HttpClient WebCilent = new HttpClient();
+// GET: SystemInformation
+
+
+
+// GET: Home
+public ActionResult Index()
+{
+IEnumerable<SystemInformation> emptList;
+
+
+
+
+string apiUrl = WebConfigurationManager.AppSettings["ApiUrl"];
+
+
+
+WebCilent.BaseAddress = new Uri(apiUrl);
+
+
+
+WebCilent.DefaultRequestHeaders.Clear();
+
+
+
+WebCilent.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+
+HttpResponseMessage response = WebCilent.GetAsync("SystemInformation").Result;
+emptList = response.Content.ReadAsAsync<IEnumerable<SystemInformation>>().Result;
+
+
+
+return View(emptList);
+}
+
+
+
+
+}
 }
